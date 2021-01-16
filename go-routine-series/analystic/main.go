6f67f4b65efc5f536d839bbdf0fb2052d55949af
guanyu.wu <guanyu.wu@orange.com>
2021-01-16 22:25:23 +0800

    add logConsumer

--- go-routine-series/analystic/main.go
+++ go-routine-series/analystic/main.go
@@ -8,12 +8,15 @@ import(
 	// "golang.org/x/net/html/atom"
 	// "crypto/dsa"
 	// "net"
+	"github.com/mgutz/str"
 )
 // 1 消费日志 -> chan 
 // 2 解析日志  <- chan  可以定义参数
 // 3 UV/PV 统计  <- chan 
 // 4 数据存储    chan
 
+
+const HANDLE_DIG = " /dig?"
 
 type cmdParams struct {
 	logFilePath string 
@@ -129,7 +132,50 @@ func readFileByLine (params cmdParams , logChannel chan string ){
 }
 
 func logConsumer( logChannel chan string , pvChannel, uvChannel chan urlData){
+		for logStr := range logChannel {
+			//切割字符串， 取出上报的数据
+			data := cutLogUploadData( logStr)
 
+			//uid  一般服务端生成 给cookie  类比百度的网站
+			//自己生成  模拟 md5(refer +ua)
+			hasher := m5.New()
+			hasher.Write([]byte(data.refer + data.ua ))
+			uid := hex.EncodeToString( hasher.Sum( nil))
+
+			//many parse job  can added here 
+			uData  :=  urlData{ data ,  uid }
+
+			pvChannel <- uData
+			uvChannel <- uData
+		}
+}
+
+func cutLogUploadData ( logStr string ) digData {
+		logStr = strings.TrimSpace(logStr)
+		//截取字符串 常规操作
+		pos1 := str.IndexOf(logStr , HANDLE_DIG , 0 )
+		if pos1 == -1 {
+			return digData{}
+		}
+		//找到了
+		pos1 += len(HANDLE_DIG)
+		pos2 = str.IndexOf(logStr, "HTTP/" , pos1)
+
+		d := str.Substr(logStr , pos1, pos2 - pos1)
+
+		//key val 的形式 需要url 处理一下
+		urlInfo , err := url.Parse("http://localhost/?" + d)
+		if err != nil {
+			return  digData{}
+		}
+		data := urlInfo.Query()
+
+		return digData{
+			time : data.Get("time"),
+			url : data.Get("url"),
+			refer : data.Get("refer"),
+			ua : data.Get("ua"),
+		}
 }
 
 func pvCounter ( pvChannel chan urlData, storageChannel chan storageBlock	){
